f7f088cf2e42bcdd4a1689f595eac2d354dc684a
jasonmead <jasmead@gmail.com>
2011-11-27 01:19:55 -0600

    Set up solution and class and test projects

---
+++ src/Charismatic.Tests/DynamicValueTests/ConversionTestFixture.cs
@@ -0,0 +1,45 @@
+﻿namespace Charismatic.Tests.DynamicValueTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using NUnit.Framework;
+    using FluentAssertions;
+
+    [TestFixture]
+    public class ConversionTestFixture
+    {
+        [Test]
+        public void Can_Convert_String_To_Boolean()
+        {
+            dynamic t = new DynamicValue("true");
+            bool testt = t;
+            testt.Should().BeTrue();
+
+            dynamic f = new DynamicValue("false");
+            bool testf = f;
+            testf.Should().BeFalse();
+        } 
+
+        [Test]
+        public void Can_Convert_Int_To_Boolean()
+        {
+            dynamic t = new DynamicValue(1);
+            bool tTest = t;
+            tTest.Should().BeTrue();
+
+
+            dynamic t2 = new DynamicValue(0);
+            bool t2Test = t2;
+            t2Test.Should().BeFalse();
+        }
+
+        [Test]
+        public void Can_Convert_String_To_Double()
+        {
+            dynamic d = new DynamicValue("100.53");
+            double dd = d;
+            dd.Should().Equals(100.53);
+        }
+    }
+}
---
+++ src/Charismatic/DynamicValue.cs
@@ -0,0 +1,64 @@
+﻿namespace Charismatic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Dynamic;
+
+    public class DynamicValue : DynamicObject
+    {
+        public DynamicValue(object value)
+        {
+            _rawValue = value;
+            _valueType = (_rawValue != null) ? _rawValue.GetType() : typeof(object);
+        }
+
+        private readonly object _rawValue;
+        private readonly Type _valueType;
+
+        public bool HasValue
+        {
+            get { return _rawValue != null; }
+        }
+
+        public object Value
+        {
+            get { return _rawValue;  }
+        }
+
+        public override string ToString()
+        {
+            return (_rawValue ==  null) ? base.ToString() : Convert.ToString(_rawValue);
+        }
+
+        public override bool TryConvert(ConvertBinder binder, out object result)
+        {
+            if (binder.Type == typeof(bool))
+            {
+
+            }
+            return base.TryConvert(binder, out result);
+        }
+
+        public static implicit operator bool(DynamicValue value)
+        {
+            if (!value.HasValue)
+            {
+                return false;
+            }
+
+            if (value._valueType.IsValueType)
+            {
+                return Convert.ToBoolean(value._rawValue);
+            }
+
+            bool result;
+            if (bool.TryParse(value.ToString(), out result))
+            {
+                return result;
+            }
+
+            return default(bool);
+        }
+    }
+}
